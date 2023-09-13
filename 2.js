0e35935f3ae6c32cec34a2491b42311d6f5fa887
Юлия Кольцова <yuliykoo@mail.ru>
2023-09-13 15:40:58 +0400

    Задача 2

---
+++ 2.js
@@ -0,0 +1,20 @@
+// Задача о странных числах: Напишите функцию, которая принимает число и возвращает true, если это число является странным, 
+// и false в противном случае. Странным числом считается число, которое равно сумме всех своих делителей, кроме самого себя.
+
+const getStrangeNumber = (number) => {
+  //Находим все делители(кроме самого числа) и записываем в массив
+  const divisors = [];
+  for(let i = 1; i <= number; i++) {
+    if (number % i === 0 && i != number) {
+      divisors.push(i);
+    }
+  }
+  //Суммируем все делители
+  const summDivisors = divisors.reduce((summ, number) => summ + number);
+  // вывводим в консоль true или false
+  return console.log(summDivisors === number);
+}
+
+getStrangeNumber(6);
+getStrangeNumber(13);
+
