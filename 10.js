ef36bc9a9831ed28a18e4cfc5140548c542494d4
Юлия Кольцова <yuliykoo@mail.ru>
2023-09-18 21:52:22 +0400

    Исправляет задание 10

--- 10.js
+++ 10.js
@@ -1,8 +1,25 @@
 // Реализовать функцию конвертации строки в JSON со всеми необходимыми проверками и валидациями.
 
-const stringToJson = (str) =>{
-  return new Function(`return ${str};`)(); // Здесь используем конструктор Function для выполнения строки как кода JavaScript
+// Тестовые данные
+const str = '{"name": "Kate", "age": "25" }';
+
+const fromJson = (string) => {
+  try {
+    return JSON.parse(string)
+  }
+  catch (error) {
+    return null
+  }
+} 
+
+const converted = fromJson(str);
+console.log(converted);
+
+
+// Конвертация строки в JSON без JSON.parse
+const stringToJson = (string) =>{
+  return new Function(`return ${string};`)(); // Здесь используем конструктор Function для выполнения строки как кода JavaScript
 }
 
-const convertedJson = stringToJson(object);
+const convertedJson = stringToJson(str);
 console.log(convertedJson);
