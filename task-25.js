aeb8da59533f6eb912ece1cf6376fba315e1095b
Юлия Кольцова <yuliykoo@mail.ru>
2023-09-25 18:08:16 +0400

    Добавляет комментарии

--- task-25.js
+++ task-25.js
@@ -2,11 +2,11 @@
 // добавляет его в DOM и устанавливает для него стиль с помощью CSS.
 
 const createNewElement = () => {
-  const div = document.createElement("div");
-  div.innerHTML = "<h1>Новый элемент</h1>";
-  div.style.backgroundColor = "blue";
-  div.style.color = "white"
-  document.body.append(div);
+  const div = document.createElement("div"); // создаем элемент div
+  div.innerHTML = "<h1>Новый элемент</h1>"; // добавляем содержимое div'a
+  div.style.backgroundColor = "blue"; // задаем фоновый цвет
+  div.style.color = "white"; // задаем цвет текста
+  document.body.append(div); // добавляем созданный и стилизованный div на страницу
 }
 
 createNewElement();
